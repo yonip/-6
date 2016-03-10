3d943c24acf591341f9edac2727e65a9045a3db2
yonip <yoni.pedersen@gmail.com>
2016-03-09 21:35:30 -0500

    Formatted the sliders/progress bars to look better, add volume slider

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,25 +1,17 @@
 package sample;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
-
-import java.io.File;
-import java.net.URISyntaxException;
 
 public class Controller {
     @FXML
@@ -46,46 +38,103 @@ public class Controller {
     @FXML
     private Button skip;
 
+    @FXML
+    private Slider soundvol;
+    @FXML
+    private ProgressBar soundprog;
+
+    private boolean playing;
+    Media media;
+    MediaPlayer player;
+
     /**
      * Called to initialize a controller after its root element has been
      * completely processed.
      */
     @FXML
     public void initialize() {
-        Media ac = new Media(getClass().getResource("/music/BillyBoyd-TheLastGoodbye.mp3").toString());
-        MediaPlayer m = new MediaPlayer(ac);
-        m.totalDurationProperty().addListener((ov, oldValue, newValue) -> songtime.setMax(newValue.toSeconds()));
-        m.setOnReady(() -> {
-            songtime.setMax(ac.getDuration().toSeconds());
-            songtime.setValue(0);
-            m.currentTimeProperty().addListener((observable, oldValue, newValue) -> {
-                int hrs = (int)(newValue.toHours());
-                int min = (int)(newValue.toMinutes())%60;
-                int sec = (int)(newValue.toSeconds())%60;
-                String t = ((hrs == 0) ? "" : hrs + ":");
-                t += ((t.isEmpty() && min < 10) ? "0" : "") + min + ":" + ((sec < 10) ? "0" : "") + sec;
-                time.setText(t);
+        media = new Media(getClass().getResource("/music/Billy Boyd - The Last Goodbye.mp3").toString());
+        String file = media.getSource().replace("%20", " ");
+        String name = file.substring(file.lastIndexOf("/") + 1, file.lastIndexOf(".mp3"));
+        String artist = name.substring(0,name.indexOf("-")).trim();
+        String song = name.substring(name.indexOf("-")+1).trim();
+        songName.setText("Artist: " + artist + "\nSong: " + song);
+        player = new MediaPlayer(media);
+        player.totalDurationProperty().addListener((ov, oldValue, newValue) -> songtime.setMax(newValue.toSeconds()));
+        player.setOnReady(() -> {
+            player.currentTimeProperty().addListener((observable, oldValue, newValue) -> {
+                time.setText(timeToText(newValue) + "/" + timeToText(player.getStopTime()));
                 if (!songtime.isValueChanging()) {
                     songtime.setValue(newValue.toSeconds());
                 }
             });
             songtime.valueChangingProperty().addListener((ov, wasChanging, isChanging) -> {
                 if (!isChanging) {
-                    m.seek(Duration.seconds(songtime.getValue()));
+                    player.seek(Duration.seconds(songtime.getValue()));
                 }
             });
             songtime.valueProperty().addListener((ov, oldVal, newVal) -> {
                 progress.setProgress(newVal.doubleValue() / songtime.getMax());
                 if (!songtime.isValueChanging()) {
-                    double currentTime = m.getCurrentTime().toSeconds();
+                    double currentTime = player.getCurrentTime().toSeconds();
                     if (Math.abs(currentTime - newVal.doubleValue()) > 0.5) {
-                        m.seek(Duration.seconds(newVal.doubleValue()));
+                        player.seek(Duration.seconds(newVal.doubleValue()));
                     }
                 }
             });
+            songtime.setMax(media.getDuration().toSeconds());
+            songtime.setValue(0);
 
-            m.play();
+            soundvol.valueProperty().addListener((observable, oldValue, newValue) -> {
+                soundprog.setProgress(newValue.doubleValue() / soundvol.getMax());
+                player.setVolume(newValue.doubleValue());
+            });
+            soundvol.setMax(1);
+            soundvol.setValue(0);
+
+            play();
+
             System.out.println("ready");
         });
+    }
+
+    private static String timeToText(Duration time) {
+        int hrs = (int) (time.toHours());
+        int min = (int) (time.toMinutes()) % 60;
+        int sec = (int) (time.toSeconds()) % 60;
+        String t = ((hrs == 0) ? "" : hrs + ":");
+        t += ((t.isEmpty() && min < 10) ? "0" : "") + min + ":" + ((sec < 10) ? "0" : "") + sec;
+        return t;
+    }
+
+    @FXML
+    private void playPause(ActionEvent event) {
+        if (playing) {
+            pause();
+        } else {
+            play();
+        }
+    }
+
+    @FXML
+    private void rewind(ActionEvent event) {
+        player.seek(player.getStartTime());
+    }
+
+    @FXML
+    private void skip(ActionEvent event) {
+        player.seek(player.getStopTime());
+    }
+
+    private void play() {
+        this.playing = true;
+        this.playPause.setText("||");
+        this.player.play();
+    }
+
+    private void pause() {
+        this.playing = false;
+        this.playPause.setText(">");
+        this.player.pause();
     }
 }
